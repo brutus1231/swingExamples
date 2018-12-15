60f9b4922324d44e8072edc723647ba98987530f
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-12-15 13:33:34 +0100

    menu

--- src/main/java/pl/sda/SwingMenu.java
+++ src/main/java/pl/sda/SwingMenu.java
@@ -3,26 +3,44 @@ package pl.sda;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class SwingMenu {
     private JPanel mainPanel;
+    private JLabel myLabel;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SwingMenu");
-        frame.setContentPane(new SwingMenu().mainPanel);
+
+        SwingMenu main = new SwingMenu();
+        frame.setContentPane(main.mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        addMenu(frame);
+        addMenu(frame, main.myLabel);
         frame.pack();
         frame.setSize(500, 300);
         frame.setBounds(200, 150, frame.getWidth(), frame.getHeight());
         frame.setVisible(true);
     }
 
-    private static void addMenu(JFrame frame) {
+    private static void addMenu(final JFrame frame, final JLabel myLabel2) {
         JMenuBar menuBar = new JMenuBar();
 
         JMenu fileMenu = new JMenu("File");
         JMenuItem openItem = new JMenuItem("Open");
+        //dodać action listener do openItem
+        openItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //dodać JFileChooser
+                JFileChooser fc = new JFileChooser();
+                int returnVal = fc.showOpenDialog(null);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fc.getSelectedFile();
+                    myLabel2.setText(file.getName());
+                }
+            }
+        });
+
         JMenuItem saveItem = new JMenuItem("Save");
         JMenuItem exitItem = new JMenuItem("Exit");
 
