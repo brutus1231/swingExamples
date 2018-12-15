14b55f9f6f04bed132bc1fbbb04f148ef0bdee8a
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-12-15 14:33:01 +0100

    menu

--- src/main/java/pl/sda/SwingCalculator.java
+++ src/main/java/pl/sda/SwingCalculator.java
@@ -1,13 +1,15 @@
 package pl.sda;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SwingCalculator {
     private JPanel mainPanel;
     private JTextField resultTextField;
     private JButton cButton;
     private JButton plusButton;
-    private JButton a1Button;
+    private JButton button1;
     private JButton button2;
     private JButton button3;
     private JButton button4;
@@ -21,8 +23,43 @@ public class SwingCalculator {
     private JButton divideButton;
     private JButton button0;
     private JButton commaButton;
-    private JButton button1;
+    private JButton buttonEquls;
 
+    private Integer dzialanie;
+    private Double liczba1;
+    private Double liczba2;
+
+    public SwingCalculator() {
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleNumberClicked("1");
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleNumberClicked("2");
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleNumberClicked("3");
+            }
+        });
+        cButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                resultTextField.setText("");
+            }
+        });
+    }
+
+    private void handleNumberClicked(String value) {
+        resultTextField.setText(resultTextField.getText() + value);
+        if (liczba1 == null) {
+            liczba1 = Double.valueOf(value);
+        } else {
+            liczba2 = Double.valueOf(value);
+        }
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SwingCalculator");
